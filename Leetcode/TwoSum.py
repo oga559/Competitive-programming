f55576004cddc2cda59f0ac47f83e10b29226846
oga559 <coolsky.4649@gmail.com>
2022-05-02 16:00:50 +0900

    クリア

---
+++ Leetcode/TwoSum.py
@@ -0,0 +1,9 @@
+https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for k in range(len(nums)) :
+            for i  in range(k+1, n):
+                if nums[k] + nums[i] == target  :
+                    return [k,i]
